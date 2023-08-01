881153c68f5490c1fbac2e43233a70529490b26e
Rafael Saraiva Mielczarski <Rafael.Mielczarski@ibm.com>
2023-08-01 16:11:20 -0300

    thinking of components, structure and composition

---
+++ use-popcorn/src/App.js
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import Box from "./layout/Box";
+import Header from "./components/Header/Header";
+import NumResults from "./components/Header/NumResults";
+import SearchBar from "./components/Header/SearchBar";
+import MoviesList from "./components/MoviesList";
+import WatchedMoviesList from "./components/WatchedMoviesList";
+import WatchedSummary from "./components/WatchedSummary";
+
+const tempMovieData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt0133093",
+    Title: "The Matrix",
+    Year: "1999",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt6751668",
+    Title: "Parasite",
+    Year: "2019",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+  },
+];
+
+const tempWatchedData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    runtime: 148,
+    imdbRating: 8.8,
+    userRating: 10,
+  },
+  {
+    imdbID: "tt0088763",
+    Title: "Back to the Future",
+    Year: "1985",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+    runtime: 116,
+    imdbRating: 8.5,
+    userRating: 9,
+  },
+];
+
+export default function App() {
+  const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
+
+  //Component Composition, criar componentes que sirvam como layout ou para resolver problemas de prop drilling, basicamente é a mesma funcionalidade que o children tem em algum componente reutilizavel, só que inves de passar um texto ou apenas algo que mude, voce só cria um componente que vai ser padrao, o pai, e altera o conteudo que vai dentro dele passando um componente filho
+
+  return (
+    <>
+      <Header>
+        <SearchBar />
+        <NumResults movies={movies} />
+      </Header>
+      <main className="main">
+        <Box>
+          <MoviesList movies={movies} />
+        </Box>
+        <Box>
+          <WatchedSummary watched={watched} />
+          <WatchedMoviesList watched={watched} />
+        </Box>
+      </main>
+    </>
+  );
+}
