f581e8ddaf195e2959b7aa1042a79f10923b0b54
Rafael Saraiva Mielczarski <Rafael.Mielczarski@ibm.com>
2023-08-03 14:42:23 -0300

    reusability, component as api, proptypes

--- use-popcorn/src/components/StarRating.js
+++ use-popcorn/src/components/StarRating.js
@@ -1,5 +1,6 @@
 import Star from "./Star";
 import { useState } from "react";
+import PropTypes from "prop-types";
 
 const containerStyle = {
   display: "flex",
@@ -11,35 +12,53 @@ const starContainerStyle = {
   display: "flex",
 };
 
-const textStyle = {
-  lineHeight: "1",
-  margin: "0",
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  className: PropTypes.string,
+  defaultRating: PropTypes.number,
+  onSetRating: PropTypes.func,
 };
 
 //Array vazio de 0 a 10
 //setar props com valor default
-export default function StarRating({ maxRating = 5 }) {
-  const [rating, setRating] = useState(0);
+export default function StarRating({
+  maxRating = 5,
+  color = "#fcc419",
+  size = 48,
+  className,
+  defaultRating = 0,
+  onSetRating,
+}) {
+  const textStyle = {
+    lineHeight: "1",
+    margin: "0",
+    color,
+    fontSize: `${size / 1.5}px`,
+  };
+
+  const [rating, setRating] = useState(defaultRating);
   const [tempRating, setTempRating] = useState(0);
 
   function handleRating(rating) {
     setRating(rating);
+    onSetRating(rating);
   }
 
   return (
-    <div style={containerStyle}>
+    <div style={containerStyle} className={className}>
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
-          <span>
-            <Star
-              key={i}
-              onRate={() => handleRating(i + 1)}
-              isStarFull={tempRating ? tempRating >= i + 1 : rating >= i + 1}
-              onHoverIn={() => setTempRating(i + 1)}
-              onHoverOut={() => setTempRating(0)}
-            />
-            {i + 1}
-          </span>
+          <Star
+            key={i}
+            onRate={() => handleRating(i + 1)}
+            isStarFull={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(0)}
+            color={color}
+            size={size}
+          />
         ))}
       </div>
       <p style={textStyle}>{tempRating || rating || ""}</p>
