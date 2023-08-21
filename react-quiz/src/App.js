43c07763856654c4d3a145b8304550caa81d02f6
Rafael Saraiva Mielczarski <Rafael.Mielczarski@ibm.com>
2023-08-21 10:42:24 -0300

    json-server, dispatching via props

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,9 +1,59 @@
-import DateCounter from "./components/DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./components/Header";
+import Loader from "./components/Loader";
+import Main from "./components/Main";
+import Error from "./components/Error";
+import StartScreen from "./components/StartScreen";
+import Questions from "./components/Questions";
+
+//centralizar os dados com o status da aplicação é uma forma pratica de gerenciar os dois estados, que são relacionados, em um unico lugar
+const initialState = {
+  questions: [],
+  //loading, error, ready, active, finished
+  status: "loading",
+  index: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      // com um dispatch recebemos os dados e informamos o status da aplicação
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    case "startGame":
+      return { ...state, status: "active" };
+    default:
+      throw new Error("Error");
+  }
+}
 
 export default function App() {
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  const numQuestions = questions.length;
+
+  useEffect(() => {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Questions question={questions[index]} />}
+      </Main>
     </div>
   );
 }
