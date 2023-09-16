bab348c632d7fac97044c1409241510363eaa295
Rafael Saraiva Mielczarski <Rafael.Mielczarski@ibm.com>
2023-09-16 17:43:01 -0300

    Modern Redux folder structure

--- redux-intro/README.md
+++ redux-intro/README.md
@@ -14,3 +14,12 @@ Chamado em um event handler ativa uma action -->
 Chamada a action, é feito o dispatch dela para o store -->
 O store é onde se concentram todos os reducers da aplicação, nele a action que foi dispatched vai ser pega pelo reducer que contem aquele tipo, tratando do estado atual e retornando um novo estado -->
 Esse novo estado gerado é atualizado causando uma re-renderização dos componentes que estão consumindo aquele estado
+
+## Redux Modern Folder Structure
+
+Um jeito moderno de separar os diversos Reducers é criando uma estrutura de pastas separada por _features_.Cada feature tem seu proposito e é bem especifica, nesse caso temos duas: _Accounts e Customers_. Dentro da pasta _features_, é criada a pasta especifica e que vai levar todos os componentes e demais arquivos relacionado a ela.
+
+Com Redux, criando um arquivo chamado **(nome da feature)Slice** e nele passamos o _initialState(feature)_, a função _(feature)Reducer_ e suas _Actions Functions_.
+Por convenção, o reducer é exportado como default e as actions functions somente exportadas.
+
+Então, dentro do arquivo **Store** teremos a combinação dos dois reducers criando o RootReducer e passando ele como argumento para o Global Store, que depois é exportado para ser acessivel em todas as partes da aplicação.
--- redux-intro/src/store.js
+++ redux-intro/src/store.js
@@ -1,65 +1,7 @@
 //deprecated, tem uma forma mais moderna
+import accountReducer from "./features/accounts/accountSlice";
+import customerReducer from "./features/customers/customersSlice";
 import { combineReducers, createStore } from "redux";
-
-const initialStateAccount = {
-  balance: 0,
-  loan: 0,
-  loanPurpose: "",
-};
-
-const initialStateCustomer = {
-  fullName: "",
-  nationalID: "",
-  createdAt: "",
-};
-
-//Diferença de um useReducer normal e um reducer com Redux é que normalmente se passa o state como o estado Inicial setado no código
-function accountReducer(state = initialStateAccount, action) {
-  switch (action.type) {
-    //o action name tem como padrão ser escrito primeiramente o dominio do estado e depois sua ação
-    case "account/deposit":
-      return { ...state, balance: state.balance + action.payload };
-    case "account/withdraw":
-      return { ...state, balance: state.balance - action.payload };
-    case "account/requestLoan":
-      if (state.loan > 0) return state;
-      return {
-        ...state,
-        balance: state.balance + action.payload.amount,
-        loan: action.payload.amount,
-        loanPurpose: action.payload.purpose,
-      };
-    case "account/payLoan":
-      return {
-        ...state,
-        loan: 0,
-        loanPurpose: "",
-        balance: state.balance - state.loan,
-      };
-    default:
-      //invés de lançar um error, como fazia no useReducer, o padrão é retorna o estado inicial
-      return state;
-  }
-}
-
-function customerReducer(state = initialStateCustomer, action) {
-  switch (action.type) {
-    case "customer/createCustomer":
-      return {
-        ...state,
-        fullName: action.payload.fullName,
-        nationalID: action.payload.nationalID,
-        createdAt: action.payload.createdAt,
-      };
-    case "customer/updateName":
-      return {
-        ...state,
-        fullName: action.payload,
-      };
-    default:
-      return state;
-  }
-}
 
 //Para usar as diferentes funções reducer que criamos, é necessário criar varias stores, mas não é bem assim que funciona com Redux.
 // No Redux, chamamos a função combineReducers, que recebe um objeto, esse objeto serãos os stores criados, o objeto vai receber o nome que queremos dar para cada store e seu reducer
@@ -70,70 +12,5 @@ const rootReducer = combineReducers({
 
 //criando o store é só chamar o metodo createStore e passar o reducer que quiser dentro
 const store = createStore(rootReducer);
-//chamamos o dispatch diretamente do store
-// store.dispatch({ type: "account/deposit", payload: 500 });
-// console.log(store.getState());
 
-// store.dispatch({ type: "account/withdraw", payload: 200 });
-// console.log(store.getState());
-
-// store.dispatch({
-//   type: "account/requestLoan",
-//   payload: { amount: 1000, purpose: "Buy a bike" },
-// });
-// console.log(store.getState());
-
-// store.dispatch({ type: "account/payLoan" });
-// console.log(store.getState());
-
-//Action Creater function, é uma convenção usada pelos devs quando estiverem usando o Redux. O Redux funcionaria normalmente sem essas funções.
-
-// store.dispatch({ type: "account/deposit", payload: 500 });
-
-//invés disso ⬆️, fazemos o ⬇️
-
-function deposit(amount) {
-  return { type: "account/deposit", payload: amount };
-}
-store.dispatch(deposit(500));
-console.log(store.getState());
-
-function withdraw(amount) {
-  return { type: "account/withdraw", payload: amount };
-}
-
-function requestLoan(amount, purpose) {
-  return { type: "account/requestLoan", payload: { amount, purpose } };
-}
-
-function payLoan() {
-  return { type: "account/payLoan" };
-}
-
-store.dispatch(withdraw(200));
-console.log(store.getState());
-
-store.dispatch(requestLoan(500, "Buy a Bike"));
-console.log(store.getState());
-
-store.dispatch(payLoan());
-console.log(store.getState());
-
-// Customer Action Functions
-
-function createCustomer(fullName, nationalID) {
-  return {
-    type: "customer/createCustomer",
-    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
-  };
-}
-
-function updateName(fullName) {
-  return { type: "customer/updateName", payload: fullName };
-}
-
-store.dispatch(createCustomer("Rafael", "121212"));
-console.log(store.getState());
-
-store.dispatch(updateName("Rafael Saraiva"));
-console.log(store.getState());
+export default store;
